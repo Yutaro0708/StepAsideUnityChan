18a7e762e8323ae689cf466191b5437c6ae31ef4
Yutaro0708 <archery0840@gmail.com>
2021-05-24 23:40:10 +0900

    Initial commit

---
+++ Assets/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private GameObject unitychan;
+    float it;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        this.it = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitychan.transform.position.z > it + 7.0f )
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
